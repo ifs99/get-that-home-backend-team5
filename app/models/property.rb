2524e6d67112553b3ea88b6292808b14d4132876
MaxAgui <manuelmaximo19@gmail.com>
2023-07-18 15:21:24 -0500

    CHORE: property atacche

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,5 +2,7 @@ class Property < ApplicationRecord
   validates :operation_type,:location,:price,:property_type,:bathroom,:bedroom,:area,:description, presence: true
 
   has_many :interactions
+  has_many_attached :photos
+
   belongs_to :user
 end
