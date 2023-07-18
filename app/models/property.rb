289dc8c0f1516d5fbd1e1d5391a175ee08bdbb85
MaxAgui <manuelmaximo19@gmail.com>
2023-07-18 14:02:11 -0500

    CHORE: validations and associations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,6 @@
 class Property < ApplicationRecord
+  validates :operation_type,:location,:price,:property_type,:bathroom,:bedroom,:area,:description, presence: true
+
+  has_many :interactions
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true
+  validates :type_user, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :properties
+  has_many :interactions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
