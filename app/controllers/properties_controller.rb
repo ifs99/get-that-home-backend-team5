3e31d51727ad5782608d80c4b01dcd33a17e1159
MaxAgui <manuelmaximo19@gmail.com>
2023-07-19 21:52:41 -0500

    CHORE: show update delete properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :require_login!, only: %i[ show edit create update destroy ]
-  before_action :set_property, only: %i[ show edit update destroy ]
+  # before_action :set_property, only: %i[ show edit update destroy ]
 
   # GET /properties or /properties.json
   def index
@@ -11,6 +11,7 @@ class PropertiesController < ApplicationController
   end
 
   def create
+    
     new_property = current_user.properties.new(property_params)
 
     if new_property.save
@@ -21,78 +22,34 @@ class PropertiesController < ApplicationController
     
   end
 
+  def show
+    property1 = Property.find(params[:id])
+    render json: property1
+  end
+
+  def update
+    property1 = Property.find(params[:id])
+    if property1.update(property_params)
+      render json: property1
+    else
+      render json: property1.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    property1 = Property.find(params[:id])
+    property1.destroy
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_category
-    @category = current_user.categories.find(params[:id])
-  end
+  # def set_category
+  #   @category = current_user.categories.find(params[:id])
+  # end
 
   # Only allow a trusted parameter "white list" through.
   def property_params
     params.require(:property).permit(:operation_type, :location, :price, :property_type, :maintanance,:bathroom,:petfriendly,:bedroom,:area,:description)
   end
 end
-
-#   # GET /properties/1 or /properties/1.json
-#   def show
-#   end
-
-#   # GET /properties/new
-#   def new
-#     @property = Property.new
-#   end
-
-#   # GET /properties/1/edit
-#   def edit
-#   end
-
-#   # POST /properties or /properties.json
-#   def create
-#     @property = Property.new(property_params)
-
-#     respond_to do |format|
-#       if @property.save
-#         format.html { redirect_to property_url(@property), notice: "Property was successfully created." }
-#         format.json { render :show, status: :created, location: @property }
-#       else
-#         format.html { render :new, status: :unprocessable_entity }
-#         format.json { render json: @property.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   # PATCH/PUT /properties/1 or /properties/1.json
-#   def update
-#     respond_to do |format|
-#       if @property.update(property_params)
-#         format.html { redirect_to property_url(@property), notice: "Property was successfully updated." }
-#         format.json { render :show, status: :ok, location: @property }
-#       else
-#         format.html { render :edit, status: :unprocessable_entity }
-#         format.json { render json: @property.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
-
-#   # DELETE /properties/1 or /properties/1.json
-#   def destroy
-#     @property.destroy
-
-#     respond_to do |format|
-#       format.html { redirect_to properties_url, notice: "Property was successfully destroyed." }
-#       format.json { head :no_content }
-#     end
-#   end
-
-#   private
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_property
-#       @property = Property.find(params[:id])
-#     end
-
-#     # Only allow a list of trusted parameters through.
-#     def property_params
-#       params.require(:property).permit(:operation_type, :location, :price, :property_type, :maintanance, :bathroom, :petfriendly, :bedroom, :area, :description, :user_id)
-#     end
-# end
