7df4f78e581f3f8e467c42317b092c1cff66ac28
Rossio Lachos <rossio.lachos28@gmail.com>
2023-07-20 00:22:48 -0400

    chore: endpoint favorites and contacted

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   post    '/login'    => 'sessions#create'
   delete  '/logout'   => 'sessions#destroy'
   post    '/signup'   => 'users#create'
+  post    '/favorites/:id'   => 'interactions#create_favorite'
+  patch    '/favorites/:id'   => 'interactions#update_favorite'
+  get   '/favorites'   => 'interactions#index_favorite'
 
+  post '/contacted/:id' => 'interactions#create_contacted'
+  get '/contacted' => 'interactions#index_contacted'
 end
