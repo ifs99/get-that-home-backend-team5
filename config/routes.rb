5fad39ef6d50d5445c76659609a1882b3a1611c4
Rossio Lachos <rossio.lachos28@gmail.com>
2023-07-20 21:01:37 -0400

    chore: delete to enpoints actived and favorites

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,20 @@ Rails.application.routes.draw do
   post    '/login'    => 'sessions#create'
   delete  '/logout'   => 'sessions#destroy'
   post    '/signup'   => 'users#create'
+
   post    '/favorites/:id'   => 'interactions#create_favorite'
   patch    '/favorites/:id'   => 'interactions#update_favorite'
   get   '/favorites'   => 'interactions#index_favorite'
+  delete '/favorites/:id'  => 'interactions#delete_favorite'
 
   post '/contacted/:id' => 'interactions#create_contacted'
   get '/contacted' => 'interactions#index_contacted'
 
   get '/actived' => 'interactions#index_actived'
   patch  '/actived/:id' => 'interactions#update_actived'
+  delete '/actived/:id'  => 'interactions#delete_favorite'
 
   get '/closed' => 'interactions#index_closed'
+  
 
 end
